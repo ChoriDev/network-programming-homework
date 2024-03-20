014a0ec8be8197ef65a810623a250986d7ac2f8d
Chori <yidodeveloper@gmail.com>
2024-03-20 19:56:16 +0900

    Fix: 한글 깨진 곳 다시 작성

--- ProCon/ProConEx.java
+++ ProCon/ProConEx.java
@@ -15,11 +15,11 @@ public class ProConEx {
 			conTh.interrupt();
 		} catch (InterruptedException e) {
 		}
-		System.out.println(" ������ �Һ��� ���� ���� ");
+		System.out.println(" 생산자 소비자 패턴 종료 ");
 	}
 }
 
-// ������ ������
+// 생산자 스레드
 class Producer extends Thread {
 	Queue<Integer> q;
 
@@ -28,13 +28,13 @@ class Producer extends Thread {
 	}
 
 	public void run() {
-		int item; // ��ǰ
-		for (int i = 0; i < 100; i++) { // ��ǰ 100�� ����
+		int item; // 상품
+		for (int i = 0; i < 100; i++) { // 상품 100개 생산
 			item = i;
 			synchronized (q) {
-				q.offer(item); // ��ǰ�� ť�� ����
-				System.out.println("���� : " + item);
-				q.notify(); // ������� �Һ��ڿ��� �˸�
+				q.offer(item); // 상품을 큐에 삽입
+				System.out.println("생산 : " + item);
+				q.notify(); // 대기 중인 소비자에게 알림
 			}
 			try {
 				Thread.sleep((int) (Math.random() * 10));
@@ -44,7 +44,7 @@ class Producer extends Thread {
 	}
 }
 
-// �Һ��� ������
+// 소비자 스레드
 class Consumer extends Thread {
 	Queue<Integer> q;
 	int number;
@@ -55,22 +55,22 @@ class Consumer extends Thread {
 	}
 
 	public void run() {
-		int item; // ��ǰ
-		int totalItem = 0; // ���� �Һ��� �� ��ǰ ����
+		int item; // 상품
+		int totalItem = 0; // 내가 소비한 총 상품 개수
 		while (!Thread.currentThread().isInterrupted()) {
 			synchronized (q) {
-				if (q.peek() == null) { // ť�� ��ǰ�� �ϳ��� ������
+				if (q.peek() == null) { // 큐에 상품이 하나도 없으면
 					try {
-						System.out.println("\t �Һ���" + number + " ���");
-						q.wait(); // ���
+						System.out.println("\t 소비자" + number + " 대기");
+						q.wait(); // 대기
 					} catch (InterruptedException e) {
-						System.out.println("\t �� �Һ� ���� : " + totalItem);
+						System.out.println("\t 총 소비 개수 : " + totalItem);
 						Thread.currentThread().interrupt();
 					}
 				} else {
-					item = q.poll(); // ť�� ù��° ��ǰ ��������
-					System.out.println("\t �Һ� : " + item);
-					totalItem++; // �� �Һ� ���� ����
+					item = q.poll(); // 큐의 첫번째 상품 가져오기
+					System.out.println("\t 소비 : " + item);
+					totalItem++; // 총 소비 개수 증가
 				}
 			}
 		}
